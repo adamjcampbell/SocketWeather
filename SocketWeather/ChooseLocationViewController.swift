e60ef7442660d41f066b7f9f77438e33ff53a399
Adam Campbell <adam.92.ac@gmail.com>
2020-01-02 13:32:56 +1100

    Slightly style the Collingwood button

--- SocketWeather/ChooseLocationViewController.swift
+++ SocketWeather/ChooseLocationViewController.swift
@@ -31,9 +31,12 @@ final class ChooseLocationViewController: UIViewController {
       titleLabel.font = .preferredFont(forTextStyle: .largeTitle, compatibleWith: traitCollection)
 
       let collingwoodButton = UIButton()
-      collingwoodButton.setTitle("Collingwood", for: .normal)
-      collingwoodButton.setTitleColor(.systemBlue, for: .normal)
+      collingwoodButton.setTitle("COLLINGWOOD", for: .normal)
+      collingwoodButton.setTitleColor(.label, for: .normal)
       collingwoodButton.addTarget(self, action: #selector(didTapCollingwood), for: .touchUpInside)
+      collingwoodButton.layer.cornerRadius = 4
+      collingwoodButton.layer.borderColor = UIColor.secondaryLabel.cgColor
+      collingwoodButton.layer.borderWidth = 1
 
       let bottomSpace = UIView()
 
@@ -43,7 +46,7 @@ final class ChooseLocationViewController: UIViewController {
       topSpace.height(to: bottomSpace, multiplier: 0.33)
       titleLabel.edgesToSuperview(excluding: [.top, .bottom], insets: .horizontal(32))
       titleLabel.top(to: topSpace, topSpace.bottomAnchor)
-      titleLabel.bottom(to: collingwoodButton, collingwoodButton.topAnchor)
+      titleLabel.bottom(to: collingwoodButton, collingwoodButton.topAnchor, offset: -8)
       collingwoodButton.edgesToSuperview(excluding: [.top, .bottom], insets: .horizontal(32))
       collingwoodButton.bottom(to: bottomSpace, bottomSpace.topAnchor)
       bottomSpace.edgesToSuperview(excluding: .top)
