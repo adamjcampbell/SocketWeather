e49c6d8f8b5af9a8a40df6997ae595c5b7100d04
Adam Campbell <adam.92.ac@gmail.com>
2020-01-02 12:38:15 +1100

    Add Where's your Weather title positioned on screen

--- SocketWeather/ChooseLocationViewController.swift
+++ SocketWeather/ChooseLocationViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import TinyConstraints
 
 final class ChooseLocationViewController: UIViewController {
   override func loadView() {
@@ -8,7 +9,25 @@ final class ChooseLocationViewController: UIViewController {
   private final class ChooseLocationView: UIView {
     override init(frame: CGRect) {
       super.init(frame: frame)
-      backgroundColor = .green
+
+      backgroundColor = UIColor(named: "background")
+
+      let topSpace = UIView()
+
+      let titleLabel = UILabel()
+      titleLabel.text = "Where's your weather?"
+      titleLabel.font = .preferredFont(forTextStyle: .largeTitle, compatibleWith: traitCollection)
+
+      let bottomSpace = UIView()
+
+      [topSpace, titleLabel, bottomSpace].forEach(addSubview)
+
+      topSpace.edgesToSuperview(excluding: .bottom)
+      topSpace.height(to: bottomSpace, multiplier: 0.33)
+      titleLabel.edgesToSuperview(excluding: [.top, .bottom], insets: .horizontal(32))
+      titleLabel.top(to: topSpace, topSpace.bottomAnchor)
+      titleLabel.bottom(to: bottomSpace, bottomSpace.topAnchor)
+      bottomSpace.edgesToSuperview(excluding: .top)
     }
 
     required init?(coder: NSCoder) {
